fb7cdd3dbb5229616425a9896f235a0b73a35f13
Stepan Skichko <st.skichko@gmail.com>
2023-08-10 11:53:56 +0300

    DayCalendarHead styled except dates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { AddTaskBtn } from './ChoosedDay/TasksColumnsList/TasksColumn/components/AddTaskBtn/AddTaskBtn';
 import { TaskToolbar } from './ChoosedDay/TasksColumnsList/TasksColumn/components/TaskToolbar/TaskToolbar';
+import { DayCalendarHead } from './ChoosedDay/DayCalendarHead/DayCalendarHead';
 
 export const App = () => {
   return (
     <div>
+      <DayCalendarHead />
       <TaskToolbar />
       <AddTaskBtn />
     </div>
