0eec1880915bfd274873ec6e0b182d72f71bfc96
Stepan Skichko <st.skichko@gmail.com>
2023-08-10 12:13:38 +0300

    ColumnHeadBar styled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import { AddTaskBtn } from './ChoosedDay/TasksColumnsList/TasksColumn/components/AddTaskBtn/AddTaskBtn';
 import { TaskToolbar } from './ChoosedDay/TasksColumnsList/TasksColumn/components/TaskToolbar/TaskToolbar';
 import { DayCalendarHead } from './ChoosedDay/DayCalendarHead/DayCalendarHead';
+import { ColumnHeadBar } from './ChoosedDay/TasksColumnsList/TasksColumn/components/ColumnHeadBar/ColumnHeadBar';
 
 export const App = () => {
   return (
     <div>
       <DayCalendarHead />
+      <ColumnHeadBar />
+
       <TaskToolbar />
       <AddTaskBtn />
     </div>
